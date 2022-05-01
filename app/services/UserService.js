ba0146dd432977f3a2a7d30c696f59b61387ed64
rosaainz <al2173040136@azc.uam.mx>
2022-05-01 17:00:11 -0500

    Metodo create (metodo estatico) - passed

---
+++ app/services/UserService.js
@@ -0,0 +1,11 @@
+const User = require('./../../app/models/User') //usaremos la clase User de la carpeta models
+
+class UserService{
+
+    //metodo estatico 
+    static create(id, username, name){
+        return new User(id, username, name, "Sin bio")
+    }
+}
+
+module.exports = UserService
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -2,7 +2,8 @@ const UserService = require('./../../app/services/UserService')
 
 describe("Test for UserService", () => {
 
-    TextDecoderStream("1. Create a new user using the UserService", () => {
+
+    test("1. Create a new user using the UserService", () => {
         const user = UserService.create(1, "carlogilmar", "Carlo")
         expect(user.username).toBe("carlogilmar")
         expect(user.name).toBe("Carlo")
