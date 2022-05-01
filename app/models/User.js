d8bf0f68f2f81f2b31c82ada28cfa0b112f98d3a
rosaainz <al2173040136@azc.uam.mx>
2022-05-01 16:46:00 -0500

    Acomodo de directorio, solo se dejo models en app y test con los requerimientos en su clase y prueba

--- app/models/User.js
+++ app/models/User.js
@@ -1,14 +1,38 @@
 class User{
-   constructor (id, username, name, bio, dateCreated, lastUpdate){
+   constructor (id, username, name, bio){
         this.id = id
         this.username = username
         this.name = name
         this.bio = bio
-        this.dateCreated = dateCreated
-        this.lastUpdated = lastUpdate
-
+        this.dateCreated = new Date()
+        this.lastUpdated = new Date()
     }
 
+    //Getters 
+    get getUsername(){
+        return this.username
+    }
+
+    get getBio(){
+        return this.bio
+    }
+
+    get getDateCreated(){
+        return this.dateCreated
+    }
+
+    get getLastUpdated(){
+        return this.lastUpdated
+    }
+
+     //Requerimiento 4: setters
+     set setUsername(newUsername){
+        this.username = newUsername
+    }
+
+    set setBio(newBio){
+        this.bio = newBio
+    }
 }
 
 module.exports = User
--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,17 +1,33 @@
 const User = require('./../../app/models/User')
+    //Requerimiento 3
+    test('Create an User object', () => {
 
-    describe("Unit Test for User class", () => {
-        test('Create an User object', () => {
+    //codigo que se usa en la app
+    const user = new User(1, "carlogilmar", "Carlo", "Bio")
 
-            //codigo que se usa en la app
-            const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+    //validacion
+    expect(user.id).toBe(1)
+    expect(user.username).toBe("carlogilmar")
+    expect(user.name).toBe("Carlo")
+    expect(user.bio).toBe("Bio")
+    expect(user.dateCreated).not.toBeUndefined()  //verifica que el valor no sea undefine
+    expect(user.lastUpdated).not.toBeUndefined()  
+    });
 
-            //validacion
-            expect(user.id).toBe(1)
-            expect(user.username).toBe("carlogilmar")
-            expect(user.name).toBe("Carlo")
-            expect(user.bio).toBe("Bio")
-            expect(user.dateCreated).toBe("dateCreated")
-            expect(user.lastUpdated).toBe("lastUpdated")
-        });
-    })
+    test('Add getters', () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        expect(user.getUsername).toBe("carlogilmar")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    });
+
+    //Requerimiento 4 USO DE SETTERS
+    test('Add setters', () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        user.setUsername = "Gilmar"
+        expect(user.username).toBe("Gilmar")
+
+        user.setBio = "New bio"
+        expect(user.bio).toBe("New bio")
+    });
