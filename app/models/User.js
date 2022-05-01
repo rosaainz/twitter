90ce49222b69713f085f81b0a3afc72f963169c0
rosaainz <al2173040136@azc.uam.mx>
2022-05-01 09:14:17 -0500

    6. Requerimento 1 - passed

--- app/models/User.js
+++ app/models/User.js
@@ -1,13 +1,13 @@
 class User{
-  /*  constructor (id, username, name, bio, dateCreated, lastUpdate){
+   constructor (id, username, name, bio, dateCreated, lastUpdate){
         this.id = id
         this.username = username
         this.name = name
         this.bio = bio
-        this.dateCreated = this.dateCreated
+        this.dateCreated = dateCreated
         this.lastUpdated = lastUpdate
 
-    }*/
+    }
 
 }
 
