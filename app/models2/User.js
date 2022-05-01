331a15d1d68a0c372bcb7dab6071ac0fce0b7eb1
rosaainz <al2173040136@azc.uam.mx>
2022-05-01 09:29:40 -0500

    Requerimiento 2 - falled

---
+++ app/models2/User.js
@@ -0,0 +1,14 @@
+class User{
+    constructor (id, username, name, bio, dateCreated, lastUpdate){
+         this.id = id
+         this.username = username
+         this.name = name
+         this.bio = bio
+         this.dateCreated = dateCreated
+         this.lastUpdated = lastUpdate
+ 
+     }
+ 
+ }
+ 
+ module.exports = User
