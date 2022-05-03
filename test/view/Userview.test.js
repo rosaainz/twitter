3dd6a4dd9a0af9dbd0a49dfa733ca03f66c418c1
rosaainz <al2173040136@azc.uam.mx>
2022-05-02 23:22:50 -0500

    Requerimiento 2,3 y 4

--- test/view/Userview.test.js
+++ test/view/Userview.test.js
@@ -16,4 +16,20 @@ test ("Return an error object when try to create a new user with a payload with 
     const result = Userview.createUser(payload)
     expect(result.error).toMatch(/necesitan tener un valor válido/)
 })
+
+//Requerimiento 3
+test ("Return an error object when try to create a new user with a payload with invalid properties", () => {
+    const payload = {username: "Username"}
+    const result = Userview.createUser(payload)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+})
+
+//Requerimiento 4
+test ("Return an error object when try to create a new user with a payload with invalid properties", () => {
+    const payload = {username: "username", id:1, name: "name"}
+    const result = Userview.createUser(payload)
+    expect(result.name).toBe("name")
+    expect(result.username).toBe("username")
+    expect(result.id).toBe(1)
+})
 })
