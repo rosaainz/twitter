c392037bb09ef61fc69d009c4ceec89631d7d280
rosaainz <al2173040136@azc.uam.mx>
2022-05-02 23:16:59 -0500

    Requerimiento 2 - failed

--- test/view/Userview.test.js
+++ test/view/Userview.test.js
@@ -9,4 +9,11 @@ describe("Test for UserView", () =>{
         //https://jestjs.io/docs/using-matchers#strings
         expect(result.error).toMatch(/payload no existe/)
     })
+
+//Requerimiento 2
+test ("Return an error object when try to create a new user with a payload with invalid properties", () => {
+    const payload = {username: null, name: 12, id: "id"}
+    const result = Userview.createUser(payload)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+})
 })
